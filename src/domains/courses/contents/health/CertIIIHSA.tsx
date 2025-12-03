a98e172e1ff4630b895b80cd193bbf2d28131212
yojuyoon <hannahyoon1121@gmail.com>
2025-12-03 16:30:32 +1100

    fix build issue

--- src/domains/courses/contents/health/CertIIIHSA.tsx
+++ src/domains/courses/contents/health/CertIIIHSA.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CourseProgress from '@/components/common/CourseProgress';
+// import CourseProgress from '@/components/common/CourseProgress';
 import Image from 'next/image';
 
 function CertIIIHSA() {
